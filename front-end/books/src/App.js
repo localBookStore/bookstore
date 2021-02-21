54185cbeab5404ad59a8180aede339d528fd88e2
편재호 <pyoun820@naver.com>
2021-02-21 18:27:56 +0900

    install react-fontawesome

--- front-end/books/src/App.js
+++ front-end/books/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Main from "./components/Main"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 function App() {
-  return <div>
-    Hello World
-  </div>
+  return <>
+    <Header />
+    <Main />
+    <Footer />
+  </>
 }
 
 export default App;
