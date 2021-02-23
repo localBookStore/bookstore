9be4394dc7d54a698eb0a6316cbea6661a65079d
편재호 <pyoun820@naver.com>
2021-02-23 18:21:57 +0900

    :fire: insert main page -> multiple carousel

--- front-end/books/src/components/MainPage/PickItems.js
+++ front-end/books/src/components/MainPage/PickItems.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHandPointDown } from '@fortawesome/free-solid-svg-icons'
 import { useState, useEffect } from "react"
 import axois from "axios"
 import Slider from "react-slick";
@@ -5,25 +7,42 @@ import './PickItems.css'
 
 const PickItems = () => {
   const [images, setImages] = useState(null);
-  
+  const UNSPLASH_API_KEY = process.env.REACT_APP_UNSPLASH_API_KEY
+  const URL = "https://api.unsplash.com/photos/random?w=150&h=150"
+
   useEffect(() => {
-    const getImage = () => {
-      URL = 
-      axois.get("")
-    }  
-  })
+    const getImage = async () => {
+      const { data } = await axois.get(URL, {
+        params: {
+          client_id: UNSPLASH_API_KEY,
+          count: 12,
+        }
+      })
+      setImages(data)
+    }
+    getImage()
+  }, [])
+
   const settings = {
     arrows: true,
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 3
   };
-  
-  return <div className="pick-items">
-    <Slider {...settings}>
 
+  return <div className="pick-items">
+    <h2 className="we-pick-title">동네책방의 </h2><span className="we-pick-title">Pick!</span>
+      <FontAwesomeIcon icon={faHandPointDown} style={{fontSize:"60px", color:"#CC87B1"}}/>
+
+    <Slider {...settings}>
+      {images !== null && images.map((res, idx) => {
+        const { urls: { small } } = res
+        return <div key={idx} className="random-pick-item">
+          <img src={small} alt={idx} className="pick-image" />
+        </div>
+      })}
     </Slider>
   </div>
 }
