0c0190ce8b1ba464ac2352956b408976e92575e8
DESKTOP-TVE05PC\yijaeseop <>
2021-03-01 16:41:53 +0900

    BakcEnd : 코드리팩토링

--- back-end/src/main/java/com/webservice/bookstore/domain/entity/item/ItemRepository.java
+++ back-end/src/main/java/com/webservice/bookstore/domain/entity/item/ItemRepository.java
@@ -1,6 +1,5 @@
 package com.webservice.bookstore.domain.entity.item;
 
-import com.webservice.bookstore.domain.entity.item.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItemRepository extends JpaRepository<Item,Long> {
