775bf0e01e4140fe9832ebc03063ef6e72f8848e
DESKTOP-TVE05PC\yijaeseop <>
2021-03-01 16:40:51 +0900

    BackEnd : Item 데이터 입력 파일로 입력함

--- back-end/src/test/java/com/webservice/bookstore/iteminsert/InsertItem.java
+++ back-end/src/test/java/com/webservice/bookstore/iteminsert/InsertItem.java
@@ -1,8 +1,9 @@
 package com.webservice.bookstore.iteminsert;
 
+import com.webservice.bookstore.domain.entity.category.Category;
 import com.webservice.bookstore.domain.entity.item.Item;
 import com.webservice.bookstore.json.JsonData;
-import com.webservice.bookstore.repository.ItemRepository;
+import com.webservice.bookstore.domain.entity.item.ItemRepository;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.junit.jupiter.api.Test;
@@ -17,28 +18,30 @@ public class InsertItem {
 
     @Test
     void JsonInsert() throws Exception{
-        JsonData data = new JsonData("c://input2.json");
+        JsonData data = new JsonData("c://input3.json");
         JSONArray jsonArray = data.getJsonArray();
 
         for(int i=0;i<jsonArray.size();i++){
             JSONObject jsonObject = (JSONObject)jsonArray.get(i);
             Long qunatity = (Long)jsonObject.get("quantity");
-            Integer price = Integer.parseInt((String)jsonObject.get("price"));
+            Long price = ((Long)jsonObject.get("price"));
             String author = (String)jsonObject.get("author");
             String isbn = (String)jsonObject.get("isbn");
             String name = (String)jsonObject.get("name");
             String publisher = (String)jsonObject.get("publisher");
             String description = (String)jsonObject.get("description");
-            String category = (String)jsonObject.get("category");
-
+            String imageUrl = (String)jsonObject.get("imageUrl");
+            Long category = (Long)jsonObject.get("category");
+            Category newCategory =Category.builder().id(category).build();
             Item item = Item.builder().name(name)
-                    .price(price)
+                    .price(price.intValue())
                     .publisher(publisher)
                     .author(author)
                     .isbn(isbn)
                     .description(description)
                     .quantity(qunatity.intValue())
-                    .category(null)
+                    .imageUrl(imageUrl)
+                    .category(newCategory)
                     .build();
             itemRepository.save(item);
 
