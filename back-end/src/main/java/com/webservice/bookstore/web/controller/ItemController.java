1f7a91a3802890e1434c88647498e7b9885e69c2
DESKTOP-TVE05PC\yijaeseop <>
2021-03-05 01:03:38 +0900

    BackEnd : 이달의도서 우리의 pick apick 생성 완료

---
+++ back-end/src/main/java/com/webservice/bookstore/web/controller/ItemController.java
@@ -0,0 +1,34 @@
+package com.webservice.bookstore.web.controller;
+
+import com.webservice.bookstore.domain.entity.item.Item;
+import com.webservice.bookstore.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/index")
+@Log4j2
+@RequiredArgsConstructor
+public class ItemController{
+
+    private final ItemService itemService;
+
+    @GetMapping("/thismonth")
+    public ResponseEntity<List<Item>> getThisMonthList(){
+        log.info("이달의 도서 보내기");
+        return new ResponseEntity<>(itemService.getRandomList(12), HttpStatus.OK);
+    }
+
+    @GetMapping("/wepickitem")
+    public ResponseEntity<List<Item>> getWePickItem(){
+        log.info("우리의 PICK 보내기");
+        return new ResponseEntity<>(itemService.getRandomList(12), HttpStatus.OK);
+    }
+}
