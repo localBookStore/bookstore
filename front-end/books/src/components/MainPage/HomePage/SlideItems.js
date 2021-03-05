883cff6c851356f217efeb9473846677ab47eb79
Jao <pyoun820@naver.com>
2021-03-05 18:18:17 +0900

    :fire: applying axios of index page

--- front-end/books/src/components/MainPage/HomePage/SlideItems.js
+++ front-end/books/src/components/MainPage/HomePage/SlideItems.js
@@ -1,24 +1,41 @@
 import { useHistory } from "react-router-dom"
+import { useState, useEffect } from "react"
+import axios from "axios"
 import Slider from "react-slick"
 import PrevArrow from "./CustomArrow/PrevArrow"
 import NextArrow from "./CustomArrow/NextArrow"
 import "./SlideItems.css"
 
 const SlideItems = () => {
-  const arr = ["tech", "nature", "animals"]
+  const [promoteImage, setPromoteImage] = useState(false)
   const history = useHistory();
 
   const GoItemDetail = (idx) => {
     history.push('/detail')
   }
 
+  useEffect(() => {
+    const getPromoteImage = async () => {
+      await axios.get("http://localhost:8080/api/index/image/")
+        .then(res => {
+          const {data} = res
+          setPromoteImage(data)
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+    getPromoteImage()
+  }, [])
+
+
   const settings = {
     // className: "center",
     // centerPadding: "60px",
     // centerMode: true,
-    fade:true,
+    fade: true,
     pauseOnHover: true,
-    dots:true,
+    dots: true,
     autoplay: true,
     arrows: true,
     infinite: true,
@@ -26,26 +43,26 @@ const SlideItems = () => {
     autoplaySpeed: 2500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    swipeToSlide:false,
+    swipeToSlide: false,
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />
   };
 
-  return <div> 
-  <Slider {...settings} className="slider">
-        {arr.map((value, idx) => {
-          return <button 
-          key={idx} 
-          className="slider-image" 
-          onClick={() => {GoItemDetail(idx)}}
-          >
+  return <div>
+    <Slider {...settings} className="slider">
+      {promoteImage && promoteImage.map((value, idx) => {
+        return <button
+          key={idx}
+          className="slider-image"
+          onClick={() => { GoItemDetail(idx) }}
+        >
           <img
             className="w-100"
-            src={`https://placeimg.com/720/280/${value}`}
+            src={value.imageUrl}
             alt="First slide"
           />
-          </button>
-        })}
+        </button>
+      })}
     </Slider></div>
 }
 export default SlideItems;
