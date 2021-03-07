8734da9e4a0562da1f4030072b93b23c40413140
leejiwon <leejiwon3737@gmail.com>
2021-03-07 16:38:19 +0900

    Cart : 장바구니 아이템 리스트 조회 기능 추가

--- back-end/src/main/java/com/webservice/bookstore/service/CartService.java
+++ back-end/src/main/java/com/webservice/bookstore/service/CartService.java
@@ -1,4 +1,39 @@
 package com.webservice.bookstore.service;
 
+import com.webservice.bookstore.domain.entity.cart.Cart;
+import com.webservice.bookstore.domain.entity.cart.CartRepository;
+import com.webservice.bookstore.web.dto.CartDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class CartService {
+
+    private final CartRepository cartRepository;
+
+    /*
+    장바구니 목록 조회 서비스 단계
+    */
+    public List<CartDto> findByMemberId(Long id) {
+        List<Cart> cartEntityList = cartRepository.findByMemberId(id);
+        List<CartDto> cartDtoList = new ArrayList<>();
+        for(Cart cartEntity : cartEntityList) {
+            CartDto cartDto = CartDto.builder()
+                    .id(cartEntity.getId())
+                    .member_id(cartEntity.getMember().getId())
+                    .item_id(cartEntity.getItem().getId())
+                    .price(cartEntity.getPrice())
+                    .quantity(cartEntity.getQuantity())
+                    .build();
+
+            cartDtoList.add(cartDto);
+        }
+        return cartDtoList;
+    }
 }
--- back-end/src/main/java/com/webservice/bookstore/web/controller/CartController.java
+++ back-end/src/main/java/com/webservice/bookstore/web/controller/CartController.java
@@ -1,4 +1,33 @@
 package com.webservice.bookstore.web.controller;
 
+import com.webservice.bookstore.service.CartService;
+import com.webservice.bookstore.web.dto.CartDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api")
+@RequiredArgsConstructor
 public class CartController {
+
+    private final CartService cartService;
+
+    /*
+    장바구니 목록 조회 요청 핸들러
+    */
+    @GetMapping(value = "/cart/", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<CartDto>> getCartItemList() {
+
+        // 세션에 저장된 로그인 계정 정보를 통해 장바구니 목록 조회 예정
+        List<CartDto> cartList = cartService.findByMemberId((long) 1);
+
+        return new ResponseEntity<>(cartList, HttpStatus.OK);
+    }
 }
