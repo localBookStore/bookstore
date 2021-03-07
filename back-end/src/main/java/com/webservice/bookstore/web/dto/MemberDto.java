95d3758a440cdabaf206b1139420f7cc71954f83
leejiwon <leejiwon3737@gmail.com>
2021-03-07 17:00:10 +0900

    MemberDto 클래스 추가

---
+++ back-end/src/main/java/com/webservice/bookstore/web/dto/MemberDto.java
@@ -0,0 +1,14 @@
+package com.webservice.bookstore.web.dto;
+
+public class MemberDto {
+
+    private Long id;
+    private String name;
+    private String userid;
+    private String password;
+    private String email;
+    private String address;
+    private String phone;
+    private String role;
+
+}
