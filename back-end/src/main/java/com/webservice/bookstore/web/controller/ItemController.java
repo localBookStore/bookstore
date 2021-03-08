a419bd23d3a81158edf096b8e8bdfecfed434db5
hangs0908 <hangs0908@gmail.com>
2021-03-08 15:47:14 +0900

    :zap: build.gradle Spring Hateoas 추가

---
+++ back-end/src/main/java/com/webservice/bookstore/web/controller/ItemController.java
@@ -0,0 +1,13 @@
+package com.webservice.bookstore.web.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.http.parser.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/api/")
+public class ItemController {
+}
