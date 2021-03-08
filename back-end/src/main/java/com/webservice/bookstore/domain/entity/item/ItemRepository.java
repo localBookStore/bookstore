5e993199b6941ebc5daf21f3c103b2c529ff80db
leejiwon <leejiwon3737@gmail.com>
2021-03-08 15:45:50 +0900

    Spring Data JPA 문법에 따라 ItemRepository.java에 @Param 어노테이션 추가

--- back-end/src/main/java/com/webservice/bookstore/domain/entity/item/ItemRepository.java
+++ back-end/src/main/java/com/webservice/bookstore/domain/entity/item/ItemRepository.java
@@ -2,11 +2,12 @@ package com.webservice.bookstore.domain.entity.item;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item,Long> {
 
    @Query(value = "select * from Item order by rand() limit :cnt",nativeQuery = true)
-   List<Item> getThisMonthbooks(int cnt);
+   List<Item> getThisMonthbooks(@Param("cnt") int cnt);
 }
