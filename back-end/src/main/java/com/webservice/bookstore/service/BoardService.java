73af8ef3e4af0e73fca3d514ca5affc6c1bdb4b9
DESKTOP-TVE05PC\yijaeseop <>
2021-03-16 01:18:45 +0900

    Backend : 게시판 순차적으로 보여주기 , 게시판 등록 상세페이지 완성

--- back-end/src/main/java/com/webservice/bookstore/service/BoardService.java
+++ back-end/src/main/java/com/webservice/bookstore/service/BoardService.java
@@ -1,9 +1,14 @@
 package com.webservice.bookstore.service;
 
+import com.querydsl.core.BooleanBuilder;
 import com.webservice.bookstore.domain.entity.board.Board;
 import com.webservice.bookstore.domain.entity.board.BoardRepository;
+import com.webservice.bookstore.domain.entity.member.Member;
+import com.webservice.bookstore.domain.entity.member.MemberRepository;
+import com.webservice.bookstore.web.dto.BoardDTO;
+import com.webservice.bookstore.web.dto.PageRequestDTO;
+import com.webservice.bookstore.web.dto.PageResultDTO;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -11,19 +16,39 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.function.Function;
+import java.util.stream.IntStream;
+
 @Service
 @Log4j2
 @RequiredArgsConstructor
 public class BoardService {
     private final BoardRepository boardRepository;
+    private final MemberRepository memberRepository;
 
-    public void ShowCommunityList(){
-        Pageable pageable = PageRequest.of(0,10, Sort.by("id").descending());
+    //게시판 보여주기
+    public PageResultDTO<BoardDTO,Board> pageCommunityList(PageRequestDTO pageRequestDTO){
+        Pageable pageable = pageRequestDTO.getPageable(Sort.by("id").descending());
         Page<Board> result = boardRepository.findAll(pageable);
 
-        result.stream().forEach(board -> {
-            System.out.println(board);
-        });
+        Function<Board,BoardDTO> fn = (entity->BoardDTO.entityToDTO(entity));
 
+        return new PageResultDTO<>(result,fn);
+
+    }
+
+
+    //게시판 등록
+    public void boardRegister(BoardDTO boardDTO){
+        Member member  = memberRepository.getOne(boardDTO.getMemberId());
+        Board board = BoardDTO.toEntity(boardDTO,member);
+        boardRepository.save(board);
+    }
+
+    //상세페이지 댓글은 아직 미완성
+    public BoardDTO showBoardDetailPage(Long id){
+        Board board = boardRepository.getOne(id);
+        BoardDTO dto = BoardDTO.entityToDTO(board);
+        return dto;
     }
 }
