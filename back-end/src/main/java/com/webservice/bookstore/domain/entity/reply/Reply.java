4d09fe53406523bbd29323bc98fc140acc712149
DESKTOP-TVE05PC\yijaeseop <>
2021-03-19 12:55:33 +0900

    BackEnd : Reply생성 레파지토리 생성 게시글 상세댓글 완성

--- back-end/src/main/java/com/webservice/bookstore/domain/entity/reply/Reply.java
+++ back-end/src/main/java/com/webservice/bookstore/domain/entity/reply/Reply.java
@@ -17,17 +17,26 @@ public class Reply extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String content;
+    private String content; //댓글 내용
 
-    private String userid;
+    private Long memberId; //작성자 키값
 
-    private int depth;
+    private String memberUserid; //작성자 아이디
 
-    private int replyIdx;
+    @Column(nullable = false,columnDefinition = "int(11) default 0")
+    private int depth; //깊이
+
+    @Column(nullable = false)
+    private int groupOrder; //댓글그룹 순서
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "board_id")
     private Board board;
 
+    @Column(nullable = false,columnDefinition = "bigint(20) default 0")
+    private long parent; //부모
 
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
---
+++ back-end/src/main/java/com/webservice/bookstore/domain/entity/reply/ReplyRepository.java
@@ -0,0 +1,25 @@
+package com.webservice.bookstore.domain.entity.reply;
+
+import com.webservice.bookstore.domain.entity.board.Board;
+import com.webservice.bookstore.web.dto.ReplyDTO;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ReplyRepository extends JpaRepository<Reply,Long>, QuerydslPredicateExecutor<Reply> {
+
+    //group순서중 가장 큰수
+    @Query(value = "select case when max(group_order) is null then 0 else max(group_order) end as 'group_order' " +
+            "from reply " +
+            "where board_id = :boardId",nativeQuery = true)
+    int getReplyOrder(@Param("boardId") long boardId);
+
+    @Query(value = "select * from reply where board_id = :boardId " +
+            "order by group_order asc ,depth asc ,created_date asc",nativeQuery = true)
+    List<Reply> getBoardReplyList(@Param("boardId") long boardId);
+
+
+}
