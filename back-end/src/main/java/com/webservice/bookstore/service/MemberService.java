68396639ed1fba98fd4b4fe60931daaa532a1036
leejiwon <leejiwon3737@gmail.com>
2021-03-24 21:50:41 +0900

    Member : 이메일 인증 링크 보내기 기능 추가

--- back-end/src/main/java/com/webservice/bookstore/service/MemberService.java
+++ back-end/src/main/java/com/webservice/bookstore/service/MemberService.java
@@ -7,6 +7,7 @@ import com.webservice.bookstore.util.EmailUtil;
 import com.webservice.bookstore.web.dto.SignUpRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,21 +20,29 @@ public class MemberService {
 
     private final PasswordEncoder encoder;
     private final MemberRepository memberRepository;
+    private final JavaMailSender javaMailSender;
 
     @Transactional
-    public MemberDto signup(MemberDto memberDto) {
+    public void signup(SignUpRequest signUpRequest) {
 
-        log.info("signup memberDto : " + memberDto);
+        log.info("signup memberDto : " + signUpRequest);
 
-        memberDto.setPassword(encoder.encode(memberDto.getPassword()));
-        memberDto.setRole(String.valueOf(MemberRole.USER));
-        memberDto.setProvider(String.valueOf(AuthProvider.DEFAULT));
+        String certificated = String.valueOf(EmailUtil.randomint());
 
-        Member savedMember = memberRepository.save(memberDto.toEntity());
-        savedMember.updateRefreshToken(jwtUtil.createRefreshToken(new CustomUserDetails(savedMember)));
-        memberDto = MemberDto.of(savedMember);
+        Member member = Member.builder()
+                .email(signUpRequest.getEmail())
+                .password(encoder.encode(signUpRequest.getPassword()))
+                .nickName(signUpRequest.getNickName())
+                .address(signUpRequest.getAddress())
+                .phone(signUpRequest.getPhone())
+                .provider(AuthProvider.DEFAULT)
+                .enabled(Boolean.FALSE)
+                .certificated(certificated)
+                .build();
 
-        return memberDto;
+        EmailUtil.sendEmail(javaMailSender, member.getEmail(), certificated);
+
+        memberRepository.save(member);
     }
 
 }
--- back-end/src/main/java/com/webservice/bookstore/util/EmailUtil.java
+++ back-end/src/main/java/com/webservice/bookstore/util/EmailUtil.java
@@ -1,5 +1,6 @@
 package com.webservice.bookstore.util;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 
@@ -7,7 +8,8 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.util.Random;
 
-public class RegisterUtil {
+@Log4j2
+public class EmailUtil {
 
     // 회원가입을 위한 인증번호 생성
     public static int randomint() {
@@ -19,18 +21,18 @@ public class RegisterUtil {
     }
 
     // 회원가입 요청 이메일 주소로 인증 메세지 송신
-    public static void sendEmail(JavaMailSender mailSender,
-                                 String email, String id, String auth, int dice) {
+    public static void sendEmail(JavaMailSender javaMailSender,
+                                 String email, String certificated) {
 
         String subject	= "[BookStore] 이메일 인증";
-        String from		= "ljwon3737@naver.com";
-        String content	= "%s님 맞으신가요?\n아래 링크를 통해 회원가입을 마무리할 수 있습니다!\n"
-                            + "링크: http://localhost:8080/api/emailcheck/?id=%s&email=%s&dice=%d&auth=%s";
-        String text	= String.format(content, id, id, email, dice, auth);
+        String from		= "bookstore0324@gmail.com";
+        String content	= "아래 링크를 통해 회원가입을 마무리할 수 있습니다!\n"
+                            + "링크: http://localhost:8080/api/emailcheck/?email=%s&certificated=%s";
+        String text	= String.format(content, email, certificated);
 
         try {
             // 메일 내용 넣을 객체와, 이를 도와주는 Helper 객체 생성
-            MimeMessage mail = mailSender.createMimeMessage();
+            MimeMessage mail = javaMailSender.createMimeMessage();
             MimeMessageHelper messageHelper = new MimeMessageHelper(mail, true, "UTF-8");
 
             // 메일 내용을 채워줌
@@ -39,7 +41,9 @@ public class RegisterUtil {
             messageHelper.setSubject(subject);	// 제목 세팅
             messageHelper.setText(text);	    // 내용 세팅
 
-            mailSender.send(mail);	// 메일 전송
+            javaMailSender.send(mail);	// 메일 전송
+
+            log.info("메일 전송 완료 : To. " + email);
 
         } catch (MessagingException e) {
             e.printStackTrace();
