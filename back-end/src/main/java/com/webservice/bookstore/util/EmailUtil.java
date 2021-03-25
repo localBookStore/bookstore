69647b9666dbc717806efd436af118c4ee2b2088
hangs0908 <hangs0908@gmail.com>
2021-03-25 17:08:49 +0900

    EmailUtil content 변수 수정

--- back-end/src/main/java/com/webservice/bookstore/util/EmailUtil.java
+++ back-end/src/main/java/com/webservice/bookstore/util/EmailUtil.java
@@ -26,29 +26,10 @@ public class EmailUtil {
 
         String subject	= "[BookStore] 이메일 인증";
         String from		= "bookstore0324@gmail.com";
-        StringBuffer content = new StringBuffer();
-        content.append("<!DOCTYPE html>");
-        content.append("<html>");
-        content.append("<head>");
-        content.append("</head>");
-        content.append("<body>");
-        content.append(
-                " <div" 																																																	+
-                        "	style=\"font-family: 'Apple SD Gothic Neo', 'sans-serif' !important; width: 400px; height: 600px; border-top: 4px solid #02b875; margin: 100px auto; padding: 30px 0; box-sizing: border-box;\">"		+
-                        "	<h1 style=\"margin: 0; padding: 0 5px; font-size: 28px; font-weight: 400;\">"																															+
-                        "		<span style=\"font-size: 15px; margin: 0 0 10px 3px;\">YG1110 BLOG</span><br />"																													+
-                        "		<span style=\"color: #02b875\">메일인증</span> 안내입니다."																																				+
-                        "	</h1>\n"																																																+
-                        "	<p style=\"font-size: 16px; line-height: 26px; margin-top: 50px; padding: 0 5px;\">"																													+
-                        "		<b style=\"color: #02b875\">인증 코드 : " + certificated +" </b> 버튼을 클릭하여 회원가입을 완료해 주세요.<br />"																													+
-                        "		감사합니다."																																															+
-                        "	</p>"																																																	+
-                        "	<div style=\"border-top: 1px solid #DDD; padding: 5px;\"></div>"																																		+
-                        " </div>"
-        );
-        content.append("</body>");
-        content.append("</html>");
-        String text	= String.format(content.toString(), email, certificated);
+        String content	= "이메일 인증 확인 메일입니다.\n"
+                            + "인증코드: " + certificated;
+
+        String text	= String.format(content, email, certificated);
 
         try {
             // 메일 내용 넣을 객체와, 이를 도와주는 Helper 객체 생성
