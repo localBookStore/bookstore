eeb2afb6095b646332ac02fc4abdc742546cf32f
hangs0908 <hangs0908@gmail.com>
2021-03-26 17:32:28 +0900

    회원탈퇴 구현

--- back-end/src/main/java/com/webservice/bookstore/domain/entity/member/MemberRepository.java
+++ back-end/src/main/java/com/webservice/bookstore/domain/entity/member/MemberRepository.java
@@ -1,7 +1,11 @@
 package com.webservice.bookstore.domain.entity.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
@@ -9,4 +13,9 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
 
     Boolean existsByEmail(String email);
+
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("update Member m set m.enabled = false where m.email = :email")
+    void withdraw(@Param("email") String email);
 }
--- back-end/src/main/java/com/webservice/bookstore/service/MemberService.java
+++ back-end/src/main/java/com/webservice/bookstore/service/MemberService.java
@@ -5,6 +5,7 @@ import com.webservice.bookstore.domain.entity.member.Member;
 import com.webservice.bookstore.domain.entity.member.MemberRepository;
 import com.webservice.bookstore.exception.DuplicateUserException;
 import com.webservice.bookstore.exception.SimpleFieldError;
+import com.webservice.bookstore.util.RedisUtil;
 import com.webservice.bookstore.web.dto.EmailDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -12,6 +13,8 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Log4j2
 @Service
@@ -22,6 +25,7 @@ public class MemberService {
     private final PasswordEncoder encoder;
     private final MemberRepository memberRepository;
     private final JavaMailSender javaMailSender;
+    private final RedisUtil redisUtil;
 
     @Transactional
     public void signup(EmailDto.SignUpRequest signUpRequest) {
@@ -43,10 +47,23 @@ public class MemberService {
         memberRepository.save(member);
     }
 
+    @Transactional
     public void duplicatedEmail(String email) {
         if(this.memberRepository.existsByEmail(email)) {
             throw new DuplicateUserException("사용 중인 이메일 입니다.", new SimpleFieldError("email", "사용중인 이메일"));
         }
     }
 
+    @Transactional
+    public void withdraw(String email, String password) {
+        Member member = this.memberRepository.findByEmail(email).get();
+        String encodePassword = encoder.encode(password);
+        if(password != null) {
+            if(!member.getPassword().equals(encodePassword)) {
+                throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+            }
+        }
+        redisUtil.deleteData(email);
+        this.memberRepository.withdraw(email);
+    }
 }
