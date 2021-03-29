830e927a81f6d47730a867d8c0f1a385bab403b9
leejiwon <leejiwon3737@gmail.com>
2021-03-30 00:12:22 +0900

    Cart : 1. 장바구니 삭제 기능 수정 (여러 개의 장바구니 아이템 삭제) 2. CartController 리팩토링 (api 주소 수정)

--- back-end/src/main/java/com/webservice/bookstore/web/controller/CartController.java
+++ back-end/src/main/java/com/webservice/bookstore/web/controller/CartController.java
@@ -2,21 +2,19 @@ package com.webservice.bookstore.web.controller;
 
 import com.webservice.bookstore.config.security.auth.CustomUserDetails;
 import com.webservice.bookstore.domain.entity.cart.CartLinkResource;
-import com.webservice.bookstore.domain.entity.member.Member;
 import com.webservice.bookstore.exception.UnauthorizedException;
 import com.webservice.bookstore.service.CartService;
 import com.webservice.bookstore.web.dto.CartDto;
 import com.webservice.bookstore.web.dto.ItemDto;
+import com.webservice.bookstore.web.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpClientErrorException;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
@@ -26,7 +24,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @Log4j2
 @RestController
-@RequestMapping(value = "/api")
+@RequestMapping(value = "/api/cart")
 @RequiredArgsConstructor
 public class CartController {
 
@@ -41,7 +39,7 @@ public class CartController {
     /*
     장바구니 목록 조회 요청 핸들러
     */
-    @GetMapping(value = "/cart/", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @GetMapping(value = "", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity getCartItemList(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
 
         // 세션에 저장된 로그인 계정 정보를 통해 장바구니 목록 조회 예정
@@ -68,7 +66,7 @@ public class CartController {
     /*
     장바구니 아이템 추가 요청 핸들러
     */
-    @PostMapping(value = "/cart/{item_id}")
+    @PostMapping(value = "/{item_id}")
     public ResponseEntity<CartDto> addCartItem(@PathVariable("item_id") Long item_id,
                                                @RequestBody CartDto cartDto,
                                                @AuthenticationPrincipal CustomUserDetails customUserDetails) {
@@ -92,7 +90,7 @@ public class CartController {
     /*
     장바구니 아이템 수량 업데이트 요청 핸들러
     */
-    @PatchMapping(value = "/cart/{cart_id}/")
+    @PatchMapping(value = "/{cart_id}")
     public ResponseEntity updateCartItem(@PathVariable("cart_id") Long cart_id,
                                          @RequestBody CartDto cartDto,
                                          @AuthenticationPrincipal CustomUserDetails customUserDetails) {
@@ -106,14 +104,14 @@ public class CartController {
     /*
     장바구니 아이템 삭제 요청 핸들러
     */
-    @DeleteMapping(value = "/cart/{cart_id}/")
-    public ResponseEntity deleteCartItem(@PathVariable("cart_id") Long cart_id,
+    @DeleteMapping(value = "", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity deleteCartItem(@RequestBody List<CartDto> cartDtoList,
                                          @AuthenticationPrincipal CustomUserDetails customUserDetails) {
 
         verifyAuthentication(customUserDetails);
 
-        Long id = customUserDetails.getMember().getId();
-        List<CartDto> cartList = cartService.deleteCartItem(id, cart_id);
+        MemberDto memberDto = MemberDto.builder().id(customUserDetails.getMember().getId()).build();
+        List<CartDto> cartList = cartService.deleteCartItem(memberDto, cartDtoList);
 
 //        List<CartLinkResource> emList = cartList.stream()
 //                .map(cartDto -> new CartLinkResource(cartDto,
