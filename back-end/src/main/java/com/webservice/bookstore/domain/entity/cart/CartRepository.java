5f30f50965c13402a2cbf2386d5ab89cf2010c37
leejiwon <leejiwon3737@gmail.com>
2021-03-31 19:07:40 +0900

    Orders : 주문(구매) 하기 기능 리팩토링

--- back-end/src/main/java/com/webservice/bookstore/domain/entity/cart/CartRepository.java
+++ back-end/src/main/java/com/webservice/bookstore/domain/entity/cart/CartRepository.java
@@ -10,13 +10,12 @@ import java.util.Optional;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
-    @Modifying(clearAutomatically = true)
     @Query("select c from Cart c join fetch c.item ci join fetch c.member cm")
     List<Cart> findByMemberId(Long member_id);
 
     Optional<Cart> findByMemberIdAndItemId(Long member_id, Long item_id);
 
-    @Modifying(clearAutomatically = true)
+    @Modifying
     @Query("delete from Cart c where c.id in :cartIdList")
     void deleteAllByIdInQuery(@Param("cartIdList") List<Long> cartIdList);
 }
