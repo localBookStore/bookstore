9d9585501e413c81a50f90cbd5f12b2d07a8b18a
DESKTOP-TVE05PC\yijaeseop <>
2021-04-05 14:32:11 +0900

    BackEnd : ImageDTO 생성

---
+++ back-end/src/main/java/com/webservice/bookstore/web/dto/ImageDTO.java
@@ -0,0 +1,39 @@
+package com.webservice.bookstore.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ImageDTO {
+	private String uuid;
+
+	private String fileName;
+
+	private String path;
+
+	public String getImageURL(){
+		try{
+			return URLEncoder.encode(path+"/"+uuid+"_"+ fileName,"UTF-8");
+		} catch (UnsupportedEncodingException e){
+			e.printStackTrace();
+		}
+		return "";
+	}
+	public String getThumbnailURL(){
+		try{
+			return URLEncoder.encode(path+"/s_"+uuid+"_"+ fileName, "UTF-8");
+		} catch (UnsupportedEncodingException e){
+			e.printStackTrace();
+		}
+		return "";
+	}
+
+}
