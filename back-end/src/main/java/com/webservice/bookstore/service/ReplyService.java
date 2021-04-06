6f101c55632098f34c362d4b57192f02c2368b31
DESKTOP-TVE05PC\yijaeseop <>
2021-04-07 04:22:41 +0900

    댓글 수정완료

--- back-end/src/main/java/com/webservice/bookstore/service/ReplyService.java
+++ back-end/src/main/java/com/webservice/bookstore/service/ReplyService.java
@@ -2,6 +2,8 @@ package com.webservice.bookstore.service;
 
 import com.webservice.bookstore.domain.entity.board.Board;
 import com.webservice.bookstore.domain.entity.board.BoardRepository;
+import com.webservice.bookstore.domain.entity.member.Member;
+import com.webservice.bookstore.domain.entity.member.MemberRepository;
 import com.webservice.bookstore.domain.entity.reply.Reply;
 import com.webservice.bookstore.domain.entity.reply.ReplyRepository;
 import com.webservice.bookstore.web.dto.BoardDTO;
@@ -12,7 +14,9 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.awt.font.OpenType;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -23,7 +27,7 @@ public class ReplyService {
 
     private final ReplyRepository replyRepository;
     private final BoardRepository boardRepository;
-
+    private final MemberRepository memberRepository;
     //댓글등록
     public void registerReply(ReplyDTO replyDTO){
         replyDTO.setGroupOrder(replyRepository.getReplyOrder(replyDTO.getBoardId())+1);
@@ -41,11 +45,17 @@ public class ReplyService {
 
     @Modifying
     @Transactional
-    public void changeReply(ReplyDTO replyDTO){
+    public boolean changeReply(ReplyDTO replyDTO,String loginEmail){
+
         Reply reply = replyRepository.getOne(replyDTO.getId());
+        if(loginEmail==null || loginEmail.length()==0)
+            return false;
+        if(!reply.getMemberEmail().equals(loginEmail))
+            return false;
 
         reply.setContent(replyDTO.getContent());
         replyRepository.save(reply);
+        return true;
     }
 
 }
--- back-end/src/main/java/com/webservice/bookstore/web/controller/ReplyController.java
+++ back-end/src/main/java/com/webservice/bookstore/web/controller/ReplyController.java
@@ -1,5 +1,7 @@
 package com.webservice.bookstore.web.controller;
 
+import com.webservice.bookstore.config.security.auth.CustomUserDetails;
+import com.webservice.bookstore.exception.UnauthorizedException;
 import com.webservice.bookstore.service.BoardService;
 import com.webservice.bookstore.service.ReplyService;
 import com.webservice.bookstore.web.dto.ReplyDTO;
@@ -10,6 +12,7 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Log4j2
@@ -25,12 +28,26 @@ public class ReplyController {
     @Autowired
     private final BoardService boardService;
 
-    @PostMapping("/board/reply/comment/")
+    @PostMapping({"/board/reply/comment/","/board/reply/comment"})
     public ResponseEntity<String> replyRegister(@RequestBody ReplyDTO replyDTO){
 
-        log.info("replyDto : {}", replyDTO);
+        log.info("replyDto : 댓글 등록", replyDTO);
         replyService.registerReply(replyDTO);
         return new ResponseEntity<>("success",HttpStatus.OK);
     }
+    @PutMapping("/board/reply/modify")
+    public ResponseEntity<String> replyModify(@RequestBody ReplyDTO replyDTO,
+                                              @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        if(customUserDetails == null) {
+            throw new UnauthorizedException("인증 오류가 발생했습니다.");
+        } else if(!customUserDetails.isEnabled()) {
+            throw new UnauthorizedException("계정이 잠겨있습니다. 관리자에게 문의해주시길 바랍니다.");
+        }
+        String email = customUserDetails.getMember().getEmail();
+        if(!replyService.changeReply(replyDTO,email))
+            return new ResponseEntity<>("접근할수 없는 방법입니다.",HttpStatus.FORBIDDEN);
+        return new ResponseEntity<>("success",HttpStatus.OK);
+    }
+
 }
 
