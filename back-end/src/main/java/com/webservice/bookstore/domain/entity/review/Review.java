57da4e3bc34abb26433f16cb3554340a83a7845c
DESKTOP-TVE05PC\yijaeseop <>
2021-04-07 11:56:31 +0900

    BackEnd : ReviewDTO ,Review 생성

---
+++ back-end/src/main/java/com/webservice/bookstore/domain/entity/review/Review.java
@@ -0,0 +1,38 @@
+package com.webservice.bookstore.domain.entity.review;
+
+import com.webservice.bookstore.domain.entity.BaseTimeEntity;
+import com.webservice.bookstore.domain.entity.board.Board;
+import com.webservice.bookstore.domain.entity.item.Item;
+import com.webservice.bookstore.domain.entity.member.Member;
+import com.webservice.bookstore.domain.entity.reply.Reply;
+import com.webservice.bookstore.web.dto.ReplyDTO;
+import com.webservice.bookstore.web.dto.ReviewDTO;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString(exclude = {"member","item"})
+public class Review extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer score;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+}
