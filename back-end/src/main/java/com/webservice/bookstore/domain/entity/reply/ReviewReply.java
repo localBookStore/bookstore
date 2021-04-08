93a3f366629d059fe1386e2bdc8db7a3503bd7e7
DESKTOP-TVE05PC\yijaeseop <>
2021-04-09 01:22:56 +0900

    BackEnd : reviewReply 생성

---
+++ back-end/src/main/java/com/webservice/bookstore/domain/entity/reply/ReviewReply.java
@@ -0,0 +1,32 @@
+package com.webservice.bookstore.domain.entity.reply;
+
+import com.webservice.bookstore.domain.entity.BaseTimeEntity;
+import com.webservice.bookstore.domain.entity.member.Member;
+import com.webservice.bookstore.domain.entity.review.Review;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString(exclude = "review")
+public class ReviewReply extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+
+
+}
