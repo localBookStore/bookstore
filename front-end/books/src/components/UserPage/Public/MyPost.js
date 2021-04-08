c9d8b6ec8d1acd755a2d728a9d42eddc823f7380
pyoun <pyoun820@naver.com>
2021-04-09 01:40:26 +0900

    :fire: check mypost request

--- front-end/books/src/components/UserPage/Public/MyPost.js
+++ front-end/books/src/components/UserPage/Public/MyPost.js
@@ -1,4 +1,23 @@
-const OrderList = () => {
+import { useState, useEffect } from "react"
+import {useCookies} from "react-cookie"
+import axios from "axios"
+
+const OrderList = ({}) => {
+  const [cookies] = useCookies(["token"])
+  const { token } = cookies
+
+  useEffect(() => {
+    getUserPost()
+  }, [])
+  
+  const getUserPost = () => {
+    axios.get("http://localhost:8080/api/mypage/boards", {
+      headers : { Authorization: token }
+    })
+    .then(res => console.log(res))
+    .catch(err => console.log(err.response))
+  }
+
   return <></>
 };
 export default OrderList;
