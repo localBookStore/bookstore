d5517406ad0c6ecea14babb42a16f76429dba8e1
pyoun <pyoun820@naver.com>
2021-04-09 18:01:40 +0900

    :fire: finish community feature

--- front-end/books/src/components/UserPage/Admin/UserList.js
+++ front-end/books/src/components/UserPage/Admin/UserList.js
@@ -1,18 +1,20 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import UserArticle from "./UserArticle"
 
+import { Button } from "react-bootstrap"
 import styled from "styled-components";
 
 const UserList = ({ location }) => {
-  const token = location.state.token;
-  console.log(token);
-  const [userList, setUserList] = useState(null);
+  const { token } = location.state;
+  const [userList, setUserList] = useState([]);
+  const [selectIdx, setSelectIdx] = useState(-1);
 
   useEffect(() => {
     axios.get("http://localhost:8080/api/admin/members/", {
       headers: {
         Authorization: token,
-      },
+      }
     })
     .then(res => setUserList(res.data))
     .catch((err) => console.log(err.response));
@@ -20,9 +22,15 @@ const UserList = ({ location }) => {
 
   return <Container>
     <ContainerTitle>회원 리스트입니다.</ContainerTitle>
-    {userList && userList.map((user, idx) => {
-      return <button key={idx} >{user.nickName}<br />{user.email}</button>
+    {userList.length && userList.map((user, idx) => {
+      return <Button 
+        onClick={() => setSelectIdx(idx)}
+        variant="info" 
+        key={idx} 
+        >{user.nickName}<br />{user.email}</Button>
     })}
+    {userList.length && selectIdx > -1 && 
+      <UserArticle userInfo={userList[selectIdx]} />}
 
   </Container>;
 };
--- front-end/books/src/components/UserPage/Public/MyPost.js
+++ front-end/books/src/components/UserPage/Public/MyPost.js
@@ -1,23 +1,61 @@
 import { useState, useEffect } from "react"
-import {useCookies} from "react-cookie"
+import { useCookies } from "react-cookie"
 import axios from "axios"
 
-const OrderList = ({}) => {
+import styled from "styled-components"
+
+const OrderList = () => {
+  const [articles, setArticles] = useState([])
   const [cookies] = useCookies(["token"])
   const { token } = cookies
 
-  useEffect(() => {
-    getUserPost()
-  }, [])
-  
   const getUserPost = () => {
     axios.get("http://localhost:8080/api/mypage/boards", {
       headers : { Authorization: token }
     })
-    .then(res => console.log(res))
+    .then(res => setArticles(res.data))
     .catch(err => console.log(err.response))
   }
+  useEffect(() => {
+    getUserPost()
+  }, [])
 
-  return <></>
+  return <Container>
+      <TagContainer>
+        <span>카테고리</span>
+        <span>게시글 명</span>
+        <span>내용</span>
+        <span>최근 날짜</span>
+      </TagContainer>
+    
+
+    {articles.length ? <div>{
+      articles.map((article, idx) => {
+        return <ItemContent>
+          <span>{article.category}</span>
+          <span>{article.title}</span>
+          <span>{article.content}</span>
+          <span>{article.modifiedDate}</span>
+        </ItemContent>
+      })
+      }</div> : <div>등록된 게시글이 없습니다.</div>}
+  </Container>
 };
 export default OrderList;
+
+const Container = styled.div`
+`
+const TagContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+
+  font-size: 22px;
+  font-weight: 600;
+`
+const ItemContent = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+
+
+  margin: 10px 0;
+`
