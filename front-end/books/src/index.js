0b4e758ec1096adae7736f69549609bed4e62c48
pyoun <59079426+developyoun@users.noreply.github.com>
2021-04-20 13:43:24 +0900

    :art: change axios request url

--- front-end/books/src/index.js
+++ front-end/books/src/index.js
@@ -4,10 +4,13 @@ import App from './App';
 import { BrowserRouter } from "react-router-dom"
 import './index.css';
 
+import axios from "axios"
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { StylesProvider } from "@material-ui/core"
+
+axios.defaults.baseURL = 'http://localhost:8080/';
 
 ReactDOM.render(
   <BrowserRouter>
