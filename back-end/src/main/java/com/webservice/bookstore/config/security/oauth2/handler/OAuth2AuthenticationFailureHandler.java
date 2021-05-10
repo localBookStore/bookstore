ba8f54c2699100be9cc10d45061e164a665097de
leejiwon <leejiwon3737@gmail.com>
2021-05-11 00:55:39 +0900

    oauth2 : 각 소셜 로그인 정보 조회 오류 수정 및 리팩토링

--- back-end/src/main/java/com/webservice/bookstore/config/security/oauth2/handler/OAuth2AuthenticationFailureHandler.java
+++ back-end/src/main/java/com/webservice/bookstore/config/security/oauth2/handler/OAuth2AuthenticationFailureHandler.java
@@ -27,26 +27,8 @@ public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationF
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
 
         log.info("OAuth2AuthenticationSuccessHandler.onAuthenticationFailure : 'Unauthorized'");
-
-//        response.setStatus(HttpStatus.UNAUTHORIZED.value());
-//        response.setContentType("application/json;charset=utf-8");
-//
-//        Map<String, Object> errorAttributes = new HashMap<>();
-//        errorAttributes.put("timestamp", OffsetDateTime.now().format(DateTimeFormatter.ISO_OFFSET_DATE_TIME));
-//        errorAttributes.put("status", HttpStatus.UNAUTHORIZED);
-//        errorAttributes.put("exception", exception.getMessage());
-//        errorAttributes.put("path", request.getRequestURI());
-
-        getRedirectStrategy().sendRedirect(request, response, "http://localhost:3000/oauth?error=true");
-
-//        response.getWriter().println(objectMapper.writeValueAsString(errorAttributes));
-//        response.getWriter().write(objectMapper.writeValueAsString(errorAttributes)); // response 바디에 json 으로 담기
-
-
-        // java.io.CharConversionException 에러는 tomcat에서 발생하는 예외이다.
-        // 발생하는 이유는 tomcat Encoding 설정이 ISO-8859-1 형식으로 되어있기 때문에,
-        // 별도로 tomcat 설정을 수정하거나 ServletOutputStream 객체가 아닌 위처럼 PrintWriter 객체로 넘겨주어야한다.
-        //response.getOutputStream().println(objectMapper.writeValueAsString(errorAttributes ));
+        getRedirectStrategy().sendRedirect(request, response,
+                "http://localhost:3000/oauth?error=" + exception.getMessage());
 
     }
 }
