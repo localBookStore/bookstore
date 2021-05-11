e5db066808fa181c221d161a6abd4c1bfdbc9edf
pyoun <59079426+developyoun@users.noreply.github.com>
2021-05-12 01:58:13 +0900

    :fire: finish user profile page

--- front-end/books/src/components/UserPage/Public/UserInfo.js
+++ front-end/books/src/components/UserPage/Public/UserInfo.js
@@ -2,18 +2,17 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 import { Image } from "react-bootstrap";
-import { TextField } from "@material-ui/core"
+import { TextField, Button } from "@material-ui/core"
 import styled from "styled-components";
 
 const UserInfo = ({ location: { state }}) => {
   const [user, setUser] = useState(null);
-  const [newData, setNewData] = useState({
-    newNickName: "",
-    newPassword1: null,
-    newPassword2: null,
-    image: "",
-  })
-
+  const [password, setPassword] = useState({
+    currentPassword: "",
+    newPassword: "",
+    newPassword2: "",
+  });
+  
   useEffect(() => {
     getUserInfo();
   }, []);
@@ -24,43 +23,131 @@ const UserInfo = ({ location: { state }}) => {
       .catch(err => console.log(err.response));
   };
 
+  const modifyUser = () => {
+    const {currentPassword, newPassword} = password;
+    axios.patch("api/mypage/modify", {
+      ...user,
+      currentPassword,
+      newPassword: newPassword === "" ? null : newPassword 
+    },{ headers: { Authorization: state.token }})
+      .then(() => alert("정보가 변경되었습니다."))
+      .catch(err => console.log(err.response))
+  }
+
   const fileChangeEvent = e => {
     const reader = new FileReader();
     const file = e.target.files[0];
     reader.readAsDataURL(file);
     
     reader.onloadend = () => {
-      setNewData({
-        ...newData,
-        image: reader.result,
+      setUser({
+        ...user,
+        imageUrl: reader.result,
       })
     }
   }
 
+  const eventHandler = e => {
+    const {name, value} = e.target;
+    if (name === 'currentPassword' || name === 'newPassword' || name === 'newPassword2'){
+      setPassword({
+        ...password,
+        [name]: value
+      })
+    } else{
+      setUser({
+        ...user,
+        [name]: value
+      })
+    }
+  }
+
+  
+  const submitEvent = () => {
+    const {newPassword, newPassword2} = password;
+    const regexr = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,16}$/;
+
+    if (newPassword === "" && newPassword2 === ""){
+      modifyUser();
+    } else{
+      if (regexr.test(newPassword) && newPassword === newPassword2){
+        modifyUser();
+      } else if(!regexr.test(newPassword)){
+        return alert("비밀번호는 8자이상 16자 이하로 영문 숫자 특수문자 조합으로 해주세요.")
+      } else{
+        return alert("새로운 비밀번호와 확인 비밀번호가 다릅니다.")
+      }
+    }
+  }
+
   return <>
-    { user && <Container>
+    { user && 
+    <Container>
       <Wrap>
-        <ProfileImage 
-          src="https://storage.googleapis.com/cr-resource/image/55d08c7d76975679c8749ad17fc211cf/mindcafe/700/56365ce0c143da9d10a546354f62cc75.jpg" 
-        />
-        <PostButton type="file" onChange={fileChangeEvent} />
+        <ProfileImage src={user.imageUrl} />
+        <PostButton 
+          type="file" 
+          accept='image/jpg,image/png,image/jpeg'
+          onChange={fileChangeEvent} />
       </Wrap>
-        
       <TagContainer>
-        <div style={{ margin: "0 20px", display: "flex", flexDirection: "column", justifyContent:"space-between"}}>
+        <div style={{display:"flex", justifyContent:"space-between"}}>
           <Tag>이메일</Tag>
-          <Tag>닉네임</Tag>
-          <Tag>현재 비밀번호</Tag>
-          <Tag>새로운 비밀번호</Tag>
-          <Tag>비밀번호 확인</Tag>
-        </div>
-        <div style={{ margin: "0 20px", display: "flex", flexDirection: "column", justifyContent:"space-between"}}>
           <UserContent>{user.email}</UserContent>
-          <TextBlock defaultValue={user.nickName} variant="outlined" size="small"/>
-          <TextBlock size="small" label="Current Password" />
-          <TextBlock size="small" label="New Password" />
-          <TextBlock size="small" label="New Password Confirm" />
         </div>
+
+        <div style={{display:"flex", justifyContent:"space-between"}}>
+          <Tag>닉네임</Tag>
+          <TextBlock 
+            required
+            defaultValue={user.nickName} 
+            variant="outlined" 
+            size="small"
+            name="nickName"
+            onChange={eventHandler}
+          />
+        </div>
+
+        <div style={{display:"flex", justifyContent:"space-between"}}>
+          <Tag>현재 비밀번호</Tag>
+          <TextBlock 
+            required 
+            label="Current Password"
+            type="password" 
+            size="small"
+            name="currentPassword"
+            value={password.currentPassword}
+            onChange={eventHandler}
+          />
+        </div>
+
+        <div style={{display:"flex", justifyContent:"space-between"}}>
+          <Tag>새로운 비밀번호</Tag>
+          <TextBlock 
+            type="password" 
+            size="small" 
+            label="New Password" 
+            placeholder="비밀번호를 변경하시려면 입력하세요." 
+            name="newPassword"
+            value={password.newPassword}
+            onChange={eventHandler}
+          />
+        </div>
+
+        <div style={{display:"flex", justifyContent:"space-between"}}>
+          <Tag>비밀번호 확인</Tag>
+          <TextBlock 
+            type="password" 
+            size="small"
+            label="New Password Confirm" 
+            placeholder="새로운 비밀번호와 동일하게 입력하세요." 
+            name="newPassword2"
+            value={password.newPassword2}
+            onChange={eventHandler}
+          />
+        </div>
+
+        <EditButton color="secondary" variant="contained" onClick={submitEvent}>정보수정</EditButton>
       </TagContainer>
     </Container>
   }
@@ -70,31 +157,47 @@ export default UserInfo;
 
 const Container = styled.div`
   display: flex;
+  justify-content: center;
   align-items: center;
-`;
+  width: 70vw;;
+`
+
 const Wrap = styled.div`
   text-align: center;
 `
+
 const TagContainer = styled.div`
   display: flex;
-  height: 300px;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 30vw;
 `
+
 const PostButton = styled.input`
   display: block;
   margin: 0 auto;
 `
+
 const ProfileImage = styled(Image)`
-  width: 500px;
+  height: 400px;
+  width: 400px;
+  object-fit: contain;
+  margin: 20px;
 `
+
 const Tag = styled.div`
-  
+  margin: auto 0;
 `
+
 const UserContent = styled.div`
   margin-bottom: 10px;
   font-size: 20px;
   font-weight: bold;
 `
+
 const TextBlock = styled(TextField)`
-  display: block;
-  
+  width: 280px;
+`
+const EditButton = styled(Button)`
+  margin-top: 30px;
 `
